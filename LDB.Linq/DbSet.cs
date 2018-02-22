b404f0fd1fdd9602fd7c40a230cff9ea3b4ec8b1
Maxim <Maxim@DESKTOP-QTMOFVJ>
2018-02-22 23:11:50 +0300

    DbSet complete first iteration

--- LDB.Linq/DbSet.cs
+++ LDB.Linq/DbSet.cs
@@ -12,156 +12,141 @@ namespace LDB.Linq
     /// <summary>
     /// Init table
     /// </summary>
-    /// <typeparam name="T">Table</typeparam>
-    public class DbSet<T> : IList<T> 
-        //where T : LTable, new()
+    public class DbSet<T> : IList<T>
     {
-        #region Local data base
-        private List<T> _items;
+        #region Private properties
 
+        private List<T> _items;
+        private string DbPath;
+        private IConverter Converter;
+
+        // TODO: Add IsReadOnly logic this[int index], 
+        private bool _isReadOnly = false;
+        
+        // Autoload collection
         private List<T> Items
         {
             get
             {
                 if (_items == null)
-                    _items = LoadData();
+                    _items = Converter.Deserialize<List<T>>(DbPath);
                 return _items;
             }
         }
+        #endregion
 
-        private List<T> LoadData()
-        {
-            return Converter.Deserialize<List<T>>(DbPath);
-        }
+        #region Setters
 
+        // Init storage file
         public void SetDbPath(string path, string extension)
         {
-            
-            var name = typeof(T).Name + "." + extension;// 
+            var name = typeof(T).Name + "." + extension;
             var fullName = Path.Combine(path, name);
             if (!Directory.Exists(path))
                 Directory.CreateDirectory(path);
             if (!File.Exists(fullName))
                 File.Create(fullName).Close();
-                DbPath = fullName;
-            //else
-            //    throw new Exception($"File {fullName} not found");
+            DbPath = fullName;
         }
 
-        public void SetDbConverter(IConverter converter)
-        {
-            Converter = converter;
-        }
+        // Init converter type
+        public void SetDbConverter(IConverter converter) => Converter = converter;
 
-        private string DbPath;
-        private IConverter Converter;
-
+        // Init IsReadOnly
+        public void SetIsReadOnly(bool key) => _isReadOnly = key; 
         #endregion
-
-
 
         #region IList
 
         /// <summary>
-        /// Get item by index
+        /// Get item by Index from collection
         /// </summary>
-        /// <param name="index">index</param>
-        /// <returns>Item</returns>
         public T this[int index]
         {
-            get
-            {
-                //return Items[index];
-                throw new NotImplementedException();
-            }
-            set => throw new NotImplementedException();
+            get => Items[index]; 
+            set => Items[index] = value;
         }
-
+        
         /// <summary>
-        /// Get count of items collection
+        /// Gec count of items in collection
         /// </summary>
         public int Count => Items.Count();
 
         /// <summary>
-        /// Is this collection not writable?
+        /// Check is read only collection
         /// </summary>
-        public bool IsReadOnly
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
-
+        public bool IsReadOnly => _isReadOnly;
+        
         /// <summary>
-        /// Add object to the end of collection
+        /// Add item to collection
         /// </summary>
-        /// <param name="item">inserted item</param>
         public void Add(T item)
         {
             Items.Add(item);
             Converter.Serialize(DbPath, Items);
-            //throw new NotImplementedException();
         }
-
+        
         /// <summary>
-        /// Clear all collection
+        /// Clear collection
         /// </summary>
         public void Clear()
         {
             Items.Clear();
             Converter.Serialize(DbPath, Items);
         }
-
+        
         /// <summary>
-        /// Is item exist in this collection?
+        /// Check existing item in collection
         /// </summary>
-        /// <param name="item">searched item</param>
-        /// <returns>result</returns>
         public bool Contains(T item) => Items.Contains(item);
 
+        /// <summary>
+        /// Copy list to array
+        /// </summary>
         public void CopyTo(T[] array, int arrayIndex) => Items.CopyTo(array, arrayIndex);
 
-        public IEnumerator<T> GetEnumerator() => Items.GetEnumerator();
-        //{
-        //    foreach(var item in Items)
-        //    {
-        //        yield return item;
-        //    }
-        //}
-        //=> Items.GetEnumerator();
-
+        /// <summary>
+        /// Get index by item
+        /// </summary>
         public int IndexOf(T item) => Items.IndexOf(item);
 
+        /// <summary>
+        /// Insert item to collection
+        /// </summary>
         public void Insert(int index, T item)
         {
             Items.Insert(index, item);
-            throw new NotImplementedException();
+            Converter.Serialize(DbPath, Items);
         }
 
+        /// <summary>
+        /// Delete item from collection 
+        /// </summary>
         public bool Remove(T item)
         {
-            Items.Remove(item);
-            throw new NotImplementedException();
+            bool result = Items.Remove(item);
+            Converter.Serialize(DbPath, Items);
+            return result;
         }
 
+        /// <summary>
+        /// Delete item from collection by index
+        /// </summary>
         public void RemoveAt(int index)
         {
             Items.RemoveAt(index);
-            throw new NotImplementedException();
+            Converter.Serialize(DbPath, Items);
         }
 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            //Items.geten
-            throw new NotImplementedException();
-        }
+        /// <summary>
+        /// Get enumerator from collection
+        /// </summary>
+        public IEnumerator<T> GetEnumerator() => Items.GetEnumerator();
 
-        //public static implicit operator List<T>(DbSet<T> items)
-        //{
-        //    //items.ToList();
-        //    throw new NotImplementedException();
-        //}
+        /// <summary>
+        /// Get enumerator from collection
+        /// </summary>
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
         #endregion
     }
 }
